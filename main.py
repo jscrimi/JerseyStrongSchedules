27438fc5a953de1ffae6323c349798bd27f32e42
Joe Crimi <jscrimi@wpi.edu>
2019-10-07 13:00:19 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,26 @@
+import requests
+import urllib.request
+import time
+from bs4 import BeautifulSoup
 
 def main():
-    return 0
+    url = 'https://www.jerseystrong.com/group-fitness-classes-in-nj'
+    response = requests.get(url)
+    #print(response)
+
+    soup = BeautifulSoup(response.text, "html.parser")
+
+    for i in range(17, 29):
+        one_link = soup.findAll('a')[i].get('href')
+        print(one_link)
+        downloadURL = 'https://www.jerseystrong.com' + one_link
+        urllib.request.urlretrieve(downloadURL)
+
+        time.sleep(1)
+    #for link in soup.find_all('a'):
+    #    print(link.get('href'))
+
+    return 1
 
 #Run the main function
 if __name__ == "__main__":
