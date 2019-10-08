86309f07bc74750217d6f60dcbcde407dbd1ebb1
Joe Crimi <darkeyedp7@gmail.com>
2019-10-08 18:54:47 -0400

    Change parsing algorithm
    No longer downloads PDFs to then parse, instead parses straight from websites

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import requests
+import re
+import pandas
 import urllib.request
 import time
 import PyPDF2
@@ -10,21 +12,36 @@ def main():
     #print(response)
 
     soup = BeautifulSoup(response.text, "html.parser")
+    switchScriptString = soup.find_all("script")[17]
+    #print(switchScriptString) #The 17th script on this page contains the links to the location schedules
 
-    for i in range(17, 29): #request and download all PDFs
-        one_link = soup.findAll('a')[i].get('href')
-        print(one_link)
-        filename = one_link[51:]
-        print(filename)
-        downloadURL = 'https://www.jerseystrong.com' + one_link
-        print(downloadURL)
-        urllib.request.urlretrieve(downloadURL, filename)
-        time.sleep(1)
+    #split that string into a list by window.location= and ;
+    splitString = re.split('window.location="|;', switchScriptString.string)
+    #print(splitString)
+    filteredStrings = [i for i in splitString if i.startswith('/class-schedules')]
+    filteredStrings = ([s.replace('"', '') for s in filteredStrings])
+    for i in filteredStrings:
+        print(i)
 
-    #for each pdf, open and scan in the first page
-    
 
-    return 1
+    #for i in range(17, 29): #request and download all PDFs
+    #    one_link = soup.findAll('a')[i].get('href')
+    #    print(one_link)
+    #    filename = one_link[51:]
+    #    print(filename)
+    #    downloadURL = 'https://www.jerseystrong.com' + one_link
+    #    print(downloadURL)
+    #    urllib.request.urlretrieve(downloadURL, filename)
+        #readFirstPage(filename)
+    #    time.sleep(1)
+    return 0
+
+#def readFirstPage(filename):
+#    pdfFileObj = open(filename, 'rb')
+#    pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
+#    pageObj = pdfReader.getPage(0)
+#    print(pageObj.extractText())
+#    return 1
 
 #Run the main function
 if __name__ == "__main__":
